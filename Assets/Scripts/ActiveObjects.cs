622ff491f121f3cd756df34b6ed6ace022dd2dca
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-01-21 15:50:50 +0900

    유니티0121

---
+++ Assets/Scripts/ActiveObjects.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        float x = transform.position.x;
+    }
+        
+}
