18979914226296f276bc52d443acd51beddf1ae5
oneofthelight <130127365+oneofthelight@users.noreply.github.com>
2025-02-06 15:49:06 +0900

    총쏘기

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+        
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+        
+        public Stuff(int bul, float fu)
+        {
+            bullets = bul;
+            fuel = fu;
+        }
+        
+        // 생성자
+        public Stuff ()
+        {
+            bullets = 1;
+            grenades = 1;
+            rockets = 1;
+        }
+    }
+    
+
+    // Stuff 클래스의 인스턴스(오브젝트) 생성
+    public Stuff myStuff = new Stuff(50, 5, 5);
+    
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+    
+    void Start()
+    {
+        Debug.Log(myStuff.bullets); 
+    }
+}
